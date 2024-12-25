0ea43f194758f2f528e6b654377e3f4c2317ba69
djpicard <26641712+djpicard@users.noreply.github.com>
2024-12-24 23:11:28 -0500

    Update file 12/24/2024, 11:11:27 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,20 +1,3 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        if target > nums[-1]: return len(nums)
-        if target < nums[0]: return 0
-        
-        def search(nums, target, high, low):
-            mid = (high + low)//2
-            print(f"high: {high}, low: {low}, mid: {mid}")
-            if nums[mid] == target:
-                return mid
-
-            if high < low:
-                return low
-
-            if nums[mid] > target:
-                return search(nums, target, mid -1, low)
-            if nums[mid] < target:
-                return search(nums, target, high, mid +1)
-
-        return search(nums, target, len(nums) - 1, 0)
+    def addBinary(self, a: str, b: str) -> str:
+        return f"{int(a, 2) + int(b, 2):b}"
