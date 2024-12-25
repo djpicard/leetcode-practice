c1a5d834ad43fd5f75d7a6efe5bb33615f2e9f92
djpicard <26641712+djpicard@users.noreply.github.com>
2024-12-24 22:47:47 -0500

    Update file 12/24/2024, 10:47:46 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,6 +1,19 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] == target or nums[i] > target:
-                return i
-        return len(nums)
+        if target > nums[-1]: return len(nums)
+        if target < nums[0]: return 0
+        
+        def search(nums, target, high, low):
+            mid = int((high + low)/2)
+            if nums[mid] == target:
+                return mid
+
+            if high - low == 1:
+                return high
+
+            if nums[mid] > target:
+                return search(nums, target, mid, low)
+            if nums[mid] < target:
+                return search(nums, target, high, mid)
+
+        return search(nums, target, len(nums) - 1, 0)
