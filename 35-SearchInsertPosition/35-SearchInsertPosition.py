b428ed9ab071977b9c7a217d0ecd3230943b685f
djpicard <26641712+djpicard@users.noreply.github.com>
2024-12-24 23:01:56 -0500

    Update 35-SearchInsertPosition.py

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,12 +1,20 @@
-   def searchInsert(self, nums, target):
-        i = 0
-        j = len(nums) - 1
-        while(i <= j):
-            pivot = (i + j) // 2
-            if (nums[pivot] == target):
-                return pivot
-            elif (nums[pivot] > target):
-                j = pivot - 1
-            else:
-                i = pivot + 1
-        return i
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target > nums[-1]: return len(nums)
+        if target < nums[0]: return 0
+        
+        def search(nums, target, high, low):
+            mid = (high + low)//2
+            print(f"high: {high}, low: {low}, mid: {mid}")
+            if nums[mid] == target:
+                return mid
+
+            if high < low:
+                return low
+
+            if nums[mid] > target:
+                return search(nums, target, mid -1, low)
+            if nums[mid] < target:
+                return search(nums, target, high, mid +1)
+
+        return search(nums, target, len(nums) - 1, 0)
