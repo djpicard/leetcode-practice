774ce49740f6e5fb6ab32cc3499f9175a628fff4
djpicard <26641712+djpicard@users.noreply.github.com>
2024-12-30 09:42:09 -0500

    Update file 12/30/2024, 9:42:08 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,12 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        lowest = prices[0]
-        for i in prices:
-            if i < lowest:
-                lowest = i
-            else:
-                profit = max(profit, i - lowest)
-        return profit
-        
-
+        min_price = float('inf')
+        max_profit = 0
+        for current_price in prices:
+            min_price = min(current_price, min_price)
+            max_profit = max(max_profit, current_price - min_price)   
+        return max_profit
