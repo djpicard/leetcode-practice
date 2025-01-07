c650b0fc85ed81dbbad3e0c559fd99aef874ec71
djpicard <26641712+djpicard@users.noreply.github.com>
2025-01-07 14:27:27 -0500

    Update file 1/7/2025, 2:27:27 PM

--- 1672-RichestCustomerWealth/1672-RichestCustomerWealth.py
+++ 1672-RichestCustomerWealth/1672-RichestCustomerWealth.py
@@ -2,7 +2,5 @@ class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         out = 0
         for i in accounts:
-            tmp = sum(i)
-            if tmp > out:
-                out = tmp
+            out = max(sum(i), out)
         return out
