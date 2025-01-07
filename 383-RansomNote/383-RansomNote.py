9072e08654e2cd3fd1bf8b71a1309fe1a3df2bf9
djpicard <26641712+djpicard@users.noreply.github.com>
2025-01-07 14:16:42 -0500

    Update file 1/7/2025, 2:16:42 PM

--- 383-RansomNote/383-RansomNote.py
+++ 383-RansomNote/383-RansomNote.py
@@ -1,11 +1,1 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        rn = list(ransomNote)
-        mg = list(magazine)
-        while rn:
-            if rn[0] in mg:
-                mg.remove(rn[0])
-                rn.pop(0)
-            else:
-                return False
-        return True
+return Counter(magazine) >= Counter(ransomNote)
